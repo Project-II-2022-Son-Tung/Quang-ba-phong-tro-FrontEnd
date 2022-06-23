c56dbaa8306c2c82e5ffdf28873d290c9577c423
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2022-06-23 16:43:14 +0700

    update

--- web/src/layouts/CustomerLayout/CustomerLayout.js
+++ web/src/layouts/CustomerLayout/CustomerLayout.js
@@ -2,24 +2,24 @@ import { Link, routes } from '@redwoodjs/router'
 
 const CustomerLayout = ({ children }) => {
   return (
-    <>
-      <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <header style={{backgroundColor:'green'}}>
+        <h1 style={{textAlign: 'center'}}>
+          <Link to={routes.home()}>House Rental</Link>
         </h1>
-        <nav>
-          <ul>
+        <nav >
+          <ul style={{textAlign:'center'}}>
             <li>
-              <Link to={routes.home()}>Home</Link>
+              <Link to={routes.home()}>Trang chủ</Link>
             </li>
             <li>
-              <Link to={routes.about()}>About</Link>
+              <Link to={routes.about()}>Phòng trọ đã lưu </Link>
             </li>
           </ul>
         </nav>
       </header>
       <main>{children}</main>
-    </>
+    </div>
   )
 }
 
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import { MetaTags } from '@redwoodjs/web'
+import {Button} from 'antd'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       Home
+      <Button type="primary">Button</Button>
     </>
   )
 }
